ef4ac0ad23202837363878fc9cafe5e4497c15a0
Rova <roandyjet@gmail.com>
2022-11-13 22:21:16 +0300

    login or register v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Form from "./components/Form";
+
+let userIsRegistered = false;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Form isRegistered={userIsRegistered} />
     </div>
   );
 }
 
 export default App;
+
+//Challenge: Without moving the userIsRegistered variable,
+//1. Show Login as the button text if userIsRegistered is true.
+//Show Register as the button text if userIsRegistered is false.
+//2. Only show the Confirm Password input if userIsRegistered is false.
+//Don't show it if userIsRegistered is true.
---
+++ src/components/Form.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Input from "./Input";
+
+const Form = (props) => {
+  // return props.isRegistered ? (
+  //   <form className="form">
+  //     <Input type="text" placeholder="Username" />
+  //     <Input type="password" placeholder="Password" />
+  //     <button type="submit">Signup</button>
+  //   </form>
+  // ) : (
+  //   <form className="form">
+  //     <Input type="text" placeholder="Username" />
+  //     <Input type="password" placeholder="Password" />
+  //     <Input type="password" placeholder="Confirm Password" />
+  //     <button type="submit">Register</button>
+  //   </form>
+  // );
+
+  // -------------------------OR-------------------------
+  return (
+    <form className="form">
+      <Input type="text" placeholder="Username" />
+      <Input type="password" placeholder="Password" />
+      {!props.isRegistered && (
+        <Input type="password" placeholder="Confirm Password" />
+      )}
+      <button type="submit">{props.isRegistered ? "Login" : "Register"}</button>
+    </form>
+  );
+};
+
+export default Form;
